8b2553c79202b7395a9aadffd196b9ea5e556758
Marquess <gui.marqwess@gmail.com>
2024-03-28 16:49:57 -0300

    Fim Projeto Revisão

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +7,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+n1='';
+n2='';
+res=0;
+
+constructor(private alertController: AlertController) {}
+
+async AlertaDivisaoPor0() {
+  const alert = await this.alertController.create({
+    header: 'Opss... ',
+    subHeader: '',
+    message: 'Não é possivel fazer divisões por 0',
+    buttons: ['OK'],
+  });
+
+  await alert.present();
+}
+somar() {
+  this.res=parseFloat(this.n1)+parseFloat(this.n2);
+}
+subtrair() {
+  this.res=parseFloat(this.n1)-parseFloat(this.n2);
+}
+dividir() {
+  if (parseFloat(this.n2)==0){
+    this.res=0;
+    this.AlertaDivisaoPor0();
+  }
+  else{
+    this.res=parseFloat(this.n1)/parseFloat(this.n2);
+  }
+  this.res=parseFloat(this.n1)/parseFloat(this.n2);
+}
+multiplicar() {
+  this.res=parseFloat(this.n1)*parseFloat(this.n2);
+}
 
 }
